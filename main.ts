fb834f3c75359d51afa8b561bbb057601d8ecd43
mcgear <michael.gearhardt@gmail.com>
2025-05-07 22:46:40 -0600

    log

--- main.ts
+++ main.ts
@@ -2,4 +2,5 @@ import "preact/compat";
 import { start } from '@fathym/eac/runtime/server';
 import { config, configure } from './configs/eac-runtime.config.ts';
 
+console.log("_________________________________+++++++++++++++++++++++++++++++++++++++++++++________________________________________")
 await start(await config, { configure });
