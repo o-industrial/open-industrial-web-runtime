280b69c7a0f21863e03d89a44ce887688216a2dc
mcgear <michael.gearhardt@gmail.com>
2025-06-13 09:26:37 -0600

    checkpoint

--- apps/components/molecules/ConnectionManagementForm.tsx
+++ apps/components/molecules/ConnectionManagementForm.tsx
@@ -1,12 +1,15 @@
 import { JSX } from 'preact';
 import { Input } from '../atoms/forms/Input.tsx';
 import { MultiSelectCheckboxGroup } from './MultiSelectCheckboxGroup.tsx';
-import { EaCDataConnectionDetails, MultiProtocolIngestOption } from '@o-industrial/common/eac';
+import {
+  EaCAzureIoTHubDataConnectionDetails,
+  MultiProtocolIngestOption,
+} from '@o-industrial/common/eac';
 import { IngestOption } from '@o-industrial/common/types';
 
 type Props = {
-  details: Partial<EaCDataConnectionDetails>;
-  onChange: (next: Partial<EaCDataConnectionDetails>) => void;
+  details: EaCAzureIoTHubDataConnectionDetails;
+  onChange: (next: Partial<EaCAzureIoTHubDataConnectionDetails>) => void;
   ingestOptions: IngestOption[];
 };
 
@@ -15,22 +18,70 @@ export function ConnectionManagementForm({
   onChange,
   ingestOptions,
 }: Props) {
-  const handleLabelInput = (e: JSX.TargetedEvent<HTMLInputElement, Event>) => {
-    onChange({ Name: e.currentTarget.value });
-  };
+  const handleStringChange = (key: keyof EaCAzureIoTHubDataConnectionDetails) =>
+    (e: JSX.TargetedEvent<HTMLInputElement, Event>) =>
+      onChange({ ...details, [key]: e.currentTarget.value });
+
+  const handleBooleanChange = (key: keyof EaCAzureIoTHubDataConnectionDetails) =>
+    (e: JSX.TargetedEvent<HTMLInputElement, Event>) =>
+      onChange({ ...details, [key]: e.currentTarget.checked });
 
   const handleProtocolsChange = (next: string[]) => {
-    onChange({ MultiProtocolIngest: next as MultiProtocolIngestOption[] });
+    onChange({
+      ...details,
+      MultiProtocolIngest: next as MultiProtocolIngestOption[],
+    });
   };
 
   return (
-    <div class='space-y-3 pt-2'>
+    <div class='space-y-4 pt-2'>
+
+      {/* Label */}
       <Input
         label='Connection Label'
         value={details.Name ?? ''}
-        onInput={handleLabelInput}
+        onInput={handleStringChange('Name')}
       />
 
+      {/* Device ID */}
+      <Input
+        label='Device ID'
+        value={details.DeviceID ?? ''}
+        onInput={handleStringChange('DeviceID')}
+      />
+
+      {/* Is IoT Edge Device */}
+      <label class='flex items-center space-x-2'>
+        <input
+          type='checkbox'
+          checked={details.IsIoTEdge ?? false}
+          onChange={handleBooleanChange('IsIoTEdge')}
+        />
+        <span class='text-sm'>Is IoT Edge Device</span>
+      </label>
+
+      {/* Subscription ID */}
+      {/* <Input
+        label='Subscription ID'
+        value={details.SubscriptionID ?? ''}
+        onInput={handleStringChange('SubscriptionID')}
+      /> */}
+
+      {/* Resource Group */}
+      {/* <Input
+        label='Resource Group Name'
+        value={details.ResourceGroupName ?? ''}
+        onInput={handleStringChange('ResourceGroupName')}
+      /> */}
+
+      {/* IoT Hub Name */}
+      {/* <Input
+        label='IoT Hub Name'
+        value={details.IoTHubName ?? ''}
+        onInput={handleStringChange('IoTHubName')}
+      /> */}
+
+      {/* Ingest Protocols */}
       <MultiSelectCheckboxGroup
         label='Ingest Protocols'
         options={ingestOptions}
