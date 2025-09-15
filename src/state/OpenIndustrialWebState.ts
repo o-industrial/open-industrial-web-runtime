6c8f41659cf91e7a986a4500ac5bdfc598c62b85
ttrichar <ttrichardson92@gmail.com>
2025-09-15 16:01:19 -0600

    active and new workspace working

--- src/state/OpenIndustrialWebState.ts
+++ src/state/OpenIndustrialWebState.ts
@@ -4,9 +4,12 @@ import { OpenIndustrialJWTPayload } from '@o-industrial/common/types';
 import { EaCUserRecord } from '@fathym/eac';
 import { EaCUserLicense } from '@fathym/eac-licensing';
 import { EaCRefreshController } from '@fathym/eac-applications/runtime/refresh';
+import { CurrentUserManager } from '../managers/CurrentUserManager.ts';
 
 export type OpenIndustrialWebState = {
   AzureAccessToken?: () => Promise<string | undefined>;
+
+  CurrentUser: CurrentUserManager;
 
   OIClient: OpenIndustrialAPIClient;
 
